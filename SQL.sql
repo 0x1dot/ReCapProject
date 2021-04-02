2ebe1c434c33b391becd513e58a1a9c1a9afa94b
Murat Dursun <41049765+0x1dot@users.noreply.github.com>
2021-04-02 10:22:43 +0300

    Initial Commit

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,6 @@
 USE [ReCapProject]
 GO
-/****** Object:  Table [dbo].[Brands]    Script Date: 2021-03-31 01:34:44 ******/
+/****** Object:  Table [dbo].[Brands]    Script Date: 2021-04-02 10:22:04 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,21 +10,25 @@ CREATE TABLE [dbo].[Brands](
 	[BrandName] [nvarchar](100) NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Cars]    Script Date: 2021-03-31 01:34:44 ******/
+/****** Object:  Table [dbo].[Cars]    Script Date: 2021-04-02 10:22:04 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Cars](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CarId] [int] IDENTITY(1,1) NOT NULL,
 	[BrandId] [int] NOT NULL,
-	[ColorId] [int] NULL,
-	[ModelYear] [nvarchar](50) NULL,
-	[DailyPrice] [int] NULL,
-	[Description] [nvarchar](200) NULL
+	[ColorId] [int] NOT NULL,
+	[CarName] [nvarchar](200) NOT NULL,
+	[ModelYear] [nvarchar](50) NOT NULL,
+	[DailyPrice] [int] NOT NULL,
+ CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED 
+(
+	[CarId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Colors]    Script Date: 2021-03-31 01:34:44 ******/
+/****** Object:  Table [dbo].[Colors]    Script Date: 2021-04-02 10:22:04 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -33,4 +37,58 @@ CREATE TABLE [dbo].[Colors](
 	[ColorId] [int] IDENTITY(1,1) NOT NULL,
 	[ColorName] [nvarchar](100) NULL
 ) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 2021-04-02 10:22:04 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerId] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NULL,
+	[CompanyName] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rentals]    Script Date: 2021-04-02 10:22:04 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rentals](
+	[RentalId] [int] IDENTITY(1,1) NOT NULL,
+	[CarId] [int] NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[RentDate] [date] NOT NULL,
+	[ReturnDate] [date] NULL,
+ CONSTRAINT [PK_Rentals] PRIMARY KEY CLUSTERED 
+(
+	[RentalId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2021-04-02 10:22:04 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[UserId] [int] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Customers]  WITH CHECK ADD  CONSTRAINT [FK_Customers_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
+GO
+ALTER TABLE [dbo].[Customers] CHECK CONSTRAINT [FK_Customers_Users]
 GO
